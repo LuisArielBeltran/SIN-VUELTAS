6c56417028e9f0ae519a0f321a028344445fde83
Luis Ariel Beltran <63753209+LuisArielBeltran@users.noreply.github.com>
2026-08-02 19:10:22 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("📍 ¡Jaque mate! El servidor nuevo de Sin Vueltas está online.");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
