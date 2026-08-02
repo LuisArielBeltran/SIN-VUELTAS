a6d143728dc77ff9ccb8290daf49d80e4e538621
Luis Ariel Beltran <63753209+LuisArielBeltran@users.noreply.github.com>
2026-08-02 19:48:46 -0300

    Update server response message

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const { Pool } = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("📍 ¡Jaque mate! El servidor nuevo de Sin Vueltas está online.");
+    res.send("📍 ¡Jaque mate! El servidor de Sin Vueltas está online desde la carpeta correcta.");
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-
